385d1e1a8bfa172a3a2afccdc9bc403549a51494
Spiker-Ng <wudingsheng1@gmail.com>
2015-04-17 23:13:01 +0800

    updated needed function following instruction

--- transwarp/db.py
+++ transwarp/db.py
@@ -8,7 +8,7 @@ transwarp.db module is created to achieve functions below:
 
 import os, re, sys, time, uuid, socket, datetime, functools, threading, logging, collections
 
-# database engine object
+# database engine object, global vars
 class _Engine(object):
     def __init__(self, connect):
         self._connect = connect
@@ -17,7 +17,7 @@ class _Engine(object):
 
 engine = None
 
-# the object containing up and down stream of the database
+# the object containing up and down stream of the database, global vars
 class _DbCtx(threading.local):
     def __init__(self):
         self.connection = None
@@ -45,7 +45,7 @@ class _ConnectionCtx(object):
             self.should_clean = True
         return self
 
-    def __exit__(self, exctype, excval, traceback):
+    def __exit__(self, exctype, excvalue, traceback):
         global _db_ctx
         if self.should_cleanup:
             _db_ctx.cleanup()
@@ -61,4 +61,44 @@ with connection():
 def do_some_db_operation():
     pass
 '''
-#This line is added for git test
+
+class _TransactionCtx(object):
+	def __enter__(self):
+		global _db_ctx
+		self.should_close_conn = False
+		if not _db_ctx.is_init():
+			_db_ctx.init()
+			self.should_close_conn = True
+		_db_ctx.transactions = _db_ctx.transactions + 1
+		return self
+		
+	def __exit__(self, exctype, excvalue, traceback):
+		global _db_ctx
+		_db_ctx.transactions = _db_ctx.transactions - 1
+		try:
+			if _db_ctx.transactions == 0
+				if exctype is None:
+					self.commit()
+				else:
+					self.rollback()
+		finally:
+			if self.should_close_conn:
+				_db_ctx.cleanup()
+				
+	def commit(self):
+		global _db_ctx
+		try:
+			_db_ctx.connection.commit()
+		except:
+			_db_ctx.connection.rollback()
+			raise
+			
+	def rollback(self):
+		global _db_ctx
+		_db_ctx.connection.rollback()
+		
+def select():
+	pass
+	
+def update():
+	pass
